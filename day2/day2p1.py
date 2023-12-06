7d843fbd9a8c13caf5500aa0867237a072946a8a
Hajvur <igorpyrczak@outlook.com>
2023-12-06 20:27:40 +0100

    first day

---
+++ day2/day2p1.py
@@ -0,0 +1,26 @@
+def day2(input):
+    sum = 0
+    with open(input) as file:
+        counter = 1
+        for line in file:
+            flag = 1 
+            line_game_count = line.split(": ")
+
+            for x in line_game_count[1].split("; "):
+                for k in x.split(", "):
+                    elements = k.split()
+                    if elements[1] == "blue" and int(elements[0]) > 14:
+                        flag = -1
+                        break
+                    if elements[1] == "red" and int(elements[0]) > 12:
+                        flag = -1
+                        break
+                    if elements[1] == "green" and int(elements[0]) > 13:
+                        flag = -1
+                        break
+            if flag ==1:
+                sum += counter 
+            counter += 1
+            print(f"sum = {sum}")
+
+day2("input.txt")
---
+++ day2/day2p2.py
@@ -0,0 +1,34 @@
+def multiply(list):
+    nums = 1
+    for x in list:
+        nums = nums * x
+    return nums
+
+def day2(input):
+
+    sum = 0
+    with open(input) as file:
+        counter = 1
+        for line in file:
+            num_cubes = {
+                "blue" : 0,
+                "red" : 0,
+                "green" : 0
+                }
+            line_game_count = line.split(": ")
+
+            for x in line_game_count[1].split("; "):
+                for k in x.split(", "):
+                    elements = k.split()
+                    if num_cubes[elements[1]] < int(elements[0]):
+                        num_cubes[elements[1]] = int(elements[0])
+
+
+            sum = sum + multiply([x for x in num_cubes.values()])    
+            print("")
+
+    print(f"sum = {sum}")
+
+
+
+day2("input.txt")
